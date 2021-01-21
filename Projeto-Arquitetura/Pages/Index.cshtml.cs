3b78df1dff19a30b0128d49ddc2c873ec86aaeae
Amanda Sousa Bezerra <amanda.sousabez@gmail.com>
2021-01-20 22:57:57 -0200

    Adicionar arquivos de projeto.

---
+++ Projeto-Arquitetura/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Arquitetura.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Time { get; set; }
+
+        public void OnGet()
+        {
+            Time = DateTime.Now.ToShortDateString();
+        }
+    }
+}
